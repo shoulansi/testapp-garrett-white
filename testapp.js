19c6acec1c56294a8d93ae00955fc10bf77e6edf
shoulansi <garrettswhite@gmail.com>
2025-04-17 15:39:33 -0500

    original code posted

--- testapp.js
+++ testapp.js
@@ -1,1 +1,46 @@
+/*Testing code and ideas for an app while working through launchcode.
+My original idea was for a Rock Climbing event planning app.
+I will be building this and improving upon it as I learn.
+*/
 
+/*This is the original code and ideas.
+
+let location = "Climbing Gym";
+let cost = 9.99
+
+console.log(location);
+console.log(cost);
+//Using Values allow for easy understanding of events. Like the location or time. Values, Data Types, and Operations.
+
+let eventInfo = "Join us in our session at "  + location + " for just " + cost + "!";
+console.log(eventInfo);
+//Here I am using the stringing charaters together as a sort of title or description for an event that was planned. Stringing Characters Together.
+
+let maxPeople = 25
+let currentPeople = 10
+
+if(currentPeople < maxPeople) {
+    console.log("There is space for you, Welcome!");
+}else {
+    console.log("Sorry, this event is full.");
+}
+//Here I am applying if elses to allow the climber to know if there is still event space. Control Structures and Logic.
+
+let usersSigned = ["Garrett", "Bryce", "Hadyn", "Kyle", "Jeff"];
+
+for(let i = 0; i < usersSigned.length; i++) {
+    console.log(usersSigned[i] + " has signed up for the event!");
+}
+//I use loops and arrays here as a sort of way to show on the app who has already signed up! Also a little more stringing characters together at the end. Working with Loops, Building Arrays, Stringing Characters Together.
+
+let climbingRoutes = [];
+
+climbingRoutes.push("Boulder Problem");
+climbingRoutes.push("V4-V5");
+climbingRoutes.push("Trad Route");
+
+console.log("Climbing Routes in this event:", climbingRoutes);
+//Here I am using what I learned to create an empty array that I can later fill to show what the event has in store for the climber. Building and Using Arrays.
+
+//This app allows for rock climbers to create, share, and sign up for events locally. Allowing for easier access for meeting new friends, showing off your strength, and other multiple uses.
+*/
